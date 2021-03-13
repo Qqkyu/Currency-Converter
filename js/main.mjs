24b2845a8aa01eb5a1153ae56168c042615b8f26
Qqkyu <kulmisiu007@gmail.com>
2021-03-13 11:20:52 +0100

    Added API failure handling

--- js/main.mjs
+++ js/main.mjs
@@ -1,10 +1,26 @@
 import setFetchedExchangeRate from "./exchange-rates.mjs";
 
+M.AutoInit();
+
+function disableInput() {
+    document.getElementById("send-input").disabled = true;
+    document.getElementById("received-input").disabled = true;
+}
+
 function displayExchangeRate(currencyCode) {
     var exRate = sessionStorage.getItem(`${currencyCode}-ExRate`);
 
     if (exRate == null) {
+        // Failed API call, notify user and disable input
+        const msg = `Failed to fetch exchange rate for ${currencyCode}`;
+        M.toast({
+            html: msg,
+            classes: "rounded",
+        });
+        document.getElementById("exchange-rate-info-cont").innerHTML = msg;
+        disableInput();
     } else {
+        // Successful API call, display current exchange rate
         document.getElementById(
             "exchange-rate-info"
         ).innerHTML = `1 ${currencyCode} = <strong>${exRate} PLN</strong>`;
